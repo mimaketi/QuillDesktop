e49e4131507a0d74bab04db8694c0ea681f6ee71
Volker Braun <vbraun.name@gmail.com>
2012-12-16 22:37:09 +0000

    quill export works

--- src/quill/exporter/quill_exporter.py
+++ src/quill/exporter/quill_exporter.py
@@ -12,13 +12,20 @@ EXAMPLES::
     >>> import os
     >>> os.path.getsize(tmp)
     >>> import tarfile
-    >>> tarfile.TarFile(tmp).getmembers()
+    >>> info = [ (t.name, t.size) for t in tarfile.TarFile(tmp).getmembers() ]
+    >>> info.sort()
+    >>> for name, size in info: print os.path.split(name)[-1], size
+    index.quill_data 198
+    page_5f55aaab-d1a6-4485-8c60-5bb48bea2319.quill_data 91
+    page_9aa10c71-c872-4d2b-b97e-1845fd5a4cfc.quill_data 27794
+    page_b5786dad-3947-4846-a230-e084d2d9e2c0.quill_data 2255
     >>> os.unlink(tmp)
 """
 
 
 import tarfile
 import struct
+import cStringIO
 
 from quill.exporter.base2 import ExporterBase2
 
@@ -51,16 +58,21 @@ class QuillExporter(ExporterBase2):
         self._page_list = []
 
     def end_export(self):
+        index = self._pack_index()
+        self._save_index(index)
         self._tar.close()
 
     def _add(self, arcname, data):
         nb_dir = 'notebook_'+self._uuid
         name = nb_dir + '/' + arcname
-        info = self._tar.gettarinfo(arcname=name)
-        self._tar.add_file(info, data)
+        info = tarfile.TarInfo(name)
+        info.size = len(data)
+        fileobj = cStringIO.StringIO(data)
+        self._tar.addfile(info, fileobj)
 
-    def _save_index(self):
+    def _save_index(self, data):
         name = 'index.quill_data'
+        self._add(name, data)
 
     def _save_page(self, data, page):
         name = 'page_' + page.uuid() + '.quill_data'
@@ -68,6 +80,14 @@ class QuillExporter(ExporterBase2):
         self._page_list.append(page.uuid())
 
     def _pack_uuid(self, uuid):
+        r"""
+        TESTS::
+
+            >>> from quill.exporter.quill_exporter import QuillExporter
+            >>> exp = QuillExporter('')
+            >>> exp._pack_uuid('b5786dad-3947-4846-a230-e084d2d9e2c0')
+            '$\x00b5786dad-3947-4846-a230-e084d2d9e2c0'
+        """
         uuid = str(uuid)
         assert len(uuid)==36
         return struct.pack('<h', 36) + uuid
@@ -84,7 +104,6 @@ class QuillExporter(ExporterBase2):
         out = ''
         out += struct.pack('>i', 1)   # version
         color = (0xff << 24) + (line.red() << 16) + (line.green() << 8) + line.blue()
-        print color, line.red(), line.green(), line.blue()
         out += struct.pack('>I', color)
         out += struct.pack('>i', line.thickness())
         out += struct.pack('>i', 5)
@@ -107,11 +126,20 @@ class QuillExporter(ExporterBase2):
             out += struct.pack('>fff', *p)
         return out
         
-    def page(self, page):
+    def _pack_page(self, page):
+        r"""
+        TESTS::
+
+            >>> from quill.exporter.quill_exporter import QuillExporter
+            >>> exp = QuillExporter('')
+            >>> exp.uuid('b5786dad-3947-4846-a230-e084d2d9e2c0')
+            >>> exp._pack_page(sample_book.get_page(1))    # doctest: +ELLIPSIS
+            '\x00\x00\x00\x06$\x00b5786dad-3947-4846-a230-e084d2d9e...
+        """
         out = ''
         out += struct.pack('>i', 6)   # version
-        out += self._pack_uuid(self._uuid)
-        out += struct.pack('>i', 1)   # tag version
+        out += self._pack_uuid(page.uuid())
+        out += struct.pack('>i', 1)   # tagset version
 
         # TODO: save tags
         out += struct.pack('>i', 0)   # number of tags
@@ -120,7 +148,7 @@ class QuillExporter(ExporterBase2):
         out += struct.pack('>i', 0)    # dummy2
         
         # TODO: preserve background
-        out = struct.pack('>i', 0)    # paper type
+        out += struct.pack('>i', 0)    # paper type
 
         out += struct.pack('>i', len(page.images()))
         for image in page.images():
@@ -128,13 +156,30 @@ class QuillExporter(ExporterBase2):
         out += struct.pack('>i', 0)    # dummy
         out += struct.pack('>?', False)    # read only
         out += struct.pack('>f', page.aspect_ratio())
-        out += struct.pack('>i', len(page.lines()))
-        for line in page.lines():
-            out += self._pack_line(line)
         out += struct.pack('>i', len(page.strokes()))
         for stroke in page.strokes():
             out += self._pack_stroke(stroke)
+        out += struct.pack('>i', len(page.lines()))
+        for line in page.lines():
+            out += self._pack_line(line)
         out += struct.pack('>i', 0)   # dummy
         out += struct.pack('>i', 0)   # number of text boxes
+        return out
+
+    def page(self, page):
+        out = self._pack_page(page)
         self._save_page(out, page)
 
+
+    def _pack_index(self):
+        out = ''
+        out += struct.pack(">i", 4)    # version
+        out += struct.pack(">i", len(self._page_list))
+        for page_uuid in self._page_list:
+            out += self._pack_uuid(page_uuid)
+        out += struct.pack(">i", 0)   # current page
+        out += struct.pack(">h", len(self._title)) + self._title
+        out += struct.pack(">q", self._ctime)
+        out += struct.pack(">q", self._mtime)
+        out += self._pack_uuid(self._uuid)
+        return out
